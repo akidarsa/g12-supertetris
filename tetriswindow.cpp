d97a2dea515c9d07d15b2d36322c439fa1b8bf64
wgranger@purdue.edu <wgranger@purdue.edu@6350e780-c728-11de-a2e8-8d36a3108c74>
2009-11-15 20:45:32 +0000

    restored the line transfer ability

--- tetriswindow.cpp
+++ tetriswindow.cpp
@@ -139,8 +139,8 @@ using namespace std;
      /*deals with line removal*/
      connect(board, SIGNAL(linesRemovedChanged(int)),linesLcd, SLOT(display(int)));
      connect(boardTwo, SIGNAL(linesRemovedChanged(int)), linesLcd2, SLOT(display(int)));
-     connect(board, SIGNAL(timeToAddLines(TetrisShape*)), boardTwo, SLOT(addLines(TetrisShape*)));
-     connect(boardTwo, SIGNAL(timeToAddLines(TetrisShape*)), board, SLOT(addLines(TetrisShape*)));
+     connect(board, SIGNAL(addLineToBuffer(TetrisShape*)), boardTwo, SLOT(bufferLines(TetrisShape*)));
+     connect(boardTwo, SIGNAL(addLineToBuffer(TetrisShape*)), board, SLOT(bufferLines(TetrisShape*)));
      connect(board, SIGNAL(iLost(bool)), boardTwo, SLOT(gameOver(bool)));
      connect(boardTwo, SIGNAL(iLost(bool)), board, SLOT(gameOver(bool)));
      connect(board, SIGNAL(gameIsStart(bool)), this, SLOT(keyGrabStart(bool)));
